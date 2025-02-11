1ca8f658de9ef28aca290f305207079b9a554851
Kozhenkov Vladimir <kozhenkov@bk.ru>
2025-02-11 22:50:32 +0300

    Homework12_dop

---
+++ src/main/java/homeworks/homework12Addition/WrongPasswordException.java
@@ -0,0 +1,7 @@
+package homeworks.homework12Addition;
+
+public class WrongPasswordException extends RuntimeException {
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
