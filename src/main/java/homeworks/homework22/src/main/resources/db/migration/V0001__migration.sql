ca9151129b4d9129a160406b0b5b69a31f395c0e
KozhenkovVD <Kozhenkov@bk.ru>
2025-04-22 17:35:07 +0300

    homework22

---
+++ src/main/java/homeworks/homework22/src/main/resources/db/migration/V0001__migration.sql
@@ -0,0 +1,22 @@
+create table if not exists Televisions
+(
+    id    bigserial primary key,
+    producer varchar(50),
+    model  varchar(50)
+);
+
+insert into Televisions (producer, model)
+values   ('Samsung', 2)
+        ,('Hisense', 4)
+        ,('Sony', 7)
+        ,('Panasonic', 6)
+        ,('Philips', 9)
+        ,('TCL', 45)
+        ,('Xiaomi', 8)
+        ,('Haier', 15)
+        ,('Яндекс', 10)
+
+
+
+
+
